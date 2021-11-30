9d3128e3eb174a6a1aa4d8d4cf2b37739879b487
nader <nalhalabi@deloitte.com>
2021-11-30 12:53:53 +0100

    add sg

--- ec2.tf
+++ ec2.tf
@@ -1,4 +1,26 @@
 resource "aws_instance" "dev" {
-  ami           = "ami-09ce2fc392a4c0fbc"
-  instance_type = "t2.micro"
+  ami             = "ami-09ce2fc392a4c0fbc"
+  instance_type   = "t2.micro"
+  security_groups = ["all-open-sg"]
+}
+
+
+resource "aws_security_group" "sg" {
+  name = "all-open-sg"
+
+  #Incoming traffic
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] #replace it with your ip address
+  }
+
+  #Outgoing traffic
+  egress {
+    from_port   = 0
+    protocol    = "tcp"
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
